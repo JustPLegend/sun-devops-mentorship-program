472632798c486bfda14fd6efeb97703d4fd2aab0
sun <sun.justplegend@gmail.com>
2023-03-08 01:40:55 +0100

    Ispravljene drzave Portugal Porto i ispis jedno ispod drugog(nepravilno). Sljedeca skripta skripta6, pokazuje ispravan ispis PORTUGAL Porto u jednoj liniji

--- week-3-bash-scripts/chapter 13/skripta5_reading-complex-values-in-a-list.sh
+++ week-3-bash-scripts/chapter 13/skripta5_reading-complex-values-in-a-list.sh
@@ -2,7 +2,7 @@
 
 #HOW NOT TO USE THE for COMMAND 
 
-for drzava in PORTUGAL ESPANA ITALIA ESTONIA 
+for drzava in PORTUGAL Porto ESPANA Barcelona ITALIA Rim
 do 
     echo "Now going to $drzava"
 done
