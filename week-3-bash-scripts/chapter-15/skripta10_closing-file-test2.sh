2608069c707ab89f599de96fd7823269e2f2cbad
sun <sun.justplegend@gmail.com>
2023-03-14 23:36:54 +0100

    Finished chapter-15

---
+++ week-3-bash-scripts/chapter-15/skripta10_closing-file-test2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# testing closing file descriptors
+exec 3> test17file
+echo "This is a test line of data" >&3
+exec 3>&-
+cat test17file
+exec 3> test17file
+echo "This'll be bad" >&3
---
+++ week-3-bash-scripts/chapter-15/skripta1_presenting-data-STDERR.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# testing STDERR messages
+echo "This is an error" >&2
+echo "This is normal output"
